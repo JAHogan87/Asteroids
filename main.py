b7c26fc9ee006af62bd4a4ac5eb648fdc13f508b
JAHogan87 <JAHogan87@gmail.com>
2026-07-28 21:03:18 -0500

    Added X close functionality

--- main.py
+++ main.py
@@ -3,26 +3,19 @@ from constants import *
 from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen.fill("black")
+	pygame.display.flip()
     
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-        return
-
-    
-    
-    
-    while True:
-     log_state()
-     for event in pygame.event.get():
-        pass
-     
-
+	while True:
+		log_state()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 
 if __name__ == "__main__":
     main()
