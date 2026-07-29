e911a8e57786f5385b3106761aec5ec4b35ae630
JAHogan87 <JAHogan87@gmail.com>
2026-07-29 12:46:41 -0500

    Static player in center of screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,14 +9,24 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	screen.fill("black")
-	pygame.display.flip()
-    
+	clock = pygame.time.Clock()
+	dt = 0.0
+
+	player = Player (SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+
 	while True:
+		dt = clock.tick(60)/1000
 		log_state()
+		
+		
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
+		screen.fill("black")
+		player.draw(screen)
+		pygame.display.flip()
+
+		
 if __name__ == "__main__":
     main()
